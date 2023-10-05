7475b7d8fd44eabd5e0ab8e8a006147a738131b7
sakethwithanh <saketh21092@iiitd.ac.in>
2023-10-05 20:39:24 +0530

    first commit

---
+++ mHealth/mHealth/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mHealth project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mHealth.settings')
+
+application = get_asgi_application()
